974f827f5c6832243399e2b1dc70b7bfd1a6f838
brianfriederich-ga <friederich_brian@bah.com>
2018-09-27 13:06:33 -0400

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='discombibleator',
-      version="1.0",
+      version="1.1",
       packages = ['discombibleator'],
       author = "Brian Friederich",
       author_email = "briantfriederich@gmail.com",
