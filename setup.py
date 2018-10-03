9e8596f1d1077cecb888a4521f6aac4beab5375a
brianfriederich-ga <friederich_brian@bah.com>
2018-10-03 13:02:06 -0400

    Troubleshoot with CSV files

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 
 setup(name='discombibleator',
-      version="1.92",
+      version="1.96",
       packages = ['discombibleator'],
-      package_dir = {'discombibleator': 'discombibleator'},
-      package_data={'discombibleator': ['data/*.csv']},
       include_package_data=True,
       author = "Brian Friederich",
       author_email = "briantfriederich@gmail.com",
